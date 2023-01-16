91756cd174fe59fe92796f5c51cc7d49b5def174
fredrik-m-j <fredrik.vgr@gmail.com>
2023-01-17 00:37:55 +0100

    Rightsize scraparea

--- main.asm
+++ main.asm
@@ -204,12 +204,12 @@ START:
 	move.l	d0,COPPTR_GAME
 	nop
 
-						; TODO: Remove SCRAP if not needed
-	move.l	#$1000,d0			; Scrap area for blitting back-and-forth to
+	move.l	#$80,d0				; Scrap area for blitting back-and-forth to
 	move.l	#MEMF_CHIP,d1
 	bsr	agdAllocateResource
 	tst.l	d0
 	bmi	.error
+	move.l	d0,HDL_COPPTR_GAME_SCRAP
 	move.l	d0,a1
 	move.l	hAddress(a1),a1
 	move.l	a1,COPPTR_GAME_SCRAP		; Unwrap and point to CHIP ram 
@@ -319,7 +319,7 @@ START:
 	bsr	FreeMemoryForHandle
 	move.l	COPPTR_GAME,a0
 	bsr	FreeMemoryForHandle
-	move.l	COPPTR_GAME_SCRAP,a0	; TODO: Remove SCRAP if not needed
+	move.l	HDL_COPPTR_GAME_SCRAP,a0
 	bsr	FreeMemoryForHandle
 	move.l	SCRAPPTR,a0
 	bsr	FreeMemoryForHandle
@@ -368,7 +368,8 @@ SFX_BRICKSMASH_STRUCT:
 COPPTR_TOP:		dc.l	0
 COPPTR_MENU:		dc.l	0
 COPPTR_GAME:		dc.l	0
-COPPTR_GAME_SCRAP:	dc.l	0	; TODO: Remove SCRAP if not needed
+HDL_COPPTR_GAME_SCRAP:	dc.l	0
+COPPTR_GAME_SCRAP:	dc.l	0
 END_COPPTR_GAME:	dc.l	0	; Points to AFTER initial boilerplate copper setup
 END_COPPTR_GAME_TILES:	dc.l	0
 SCRAPPTR:		dc.l	0
