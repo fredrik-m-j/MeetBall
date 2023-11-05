1dcf0a2f54bba3d53f51de074ab42ea93de6d908
fredrik-m-j <fredrik.vgr@gmail.com>
2023-11-05 10:40:28 +0100

    Fix ball position

--- s/debugging/balldebug.asm
+++ s/debugging/balldebug.asm
@@ -1,13 +1,15 @@
 ; Set any start position and direction/speed.
 ReleaseBallFromPosition:
-	move.l	#16*VC_FACTOR,d0			; Starting X pos
-	move.l	#48*VC_FACTOR,d1			; Starting Y pos
-
 	; move.w  #80,BallspeedBase
 	bsr	ResetBallspeeds
 
-	move.w	BallSpeedx3,d2			; Starting X speed
-	move.w	BallSpeedx1,d3			; Starting Y speed
+	move.w	BallSpeedx2,d2			; Starting X speed
+	neg.w	d2
+	move.w	BallSpeedx2,d3			; Starting Y speed
+	neg.w	d3
+
+	move.l	#16*VC_FACTOR,d0			; Starting X pos
+	move.l	#16*VC_FACTOR,d1			; Starting Y pos
 
 	lea	Ball0,a0
 	move.w  d0,hSprBobTopLeftXPos(a0)
