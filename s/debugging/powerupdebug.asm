711d97f13f2fc8d76f2439647bb4c7b0f28ee537
fredrik-m-j <fredrik.vgr@gmail.com>
2025-01-21 21:58:09 +0100

    Bugfix

--- s/debugging/powerupdebug.asm
+++ s/debugging/powerupdebug.asm
@@ -12,12 +12,24 @@ DebugCheckAddPowerup:
 	rts
 
 DebugGlueBats:
+	tst.b	Player0Enabled
+	bmi		.p1
 	lea		Bat0,a1
 	bsr		PwrStartGluebat
+.p1
+	tst.b	Player1Enabled
+	bmi		.p2
 	lea		Bat1,a1
 	bsr		PwrStartGluebat
+.p2
+	tst.b	Player2Enabled
+	bmi		.p3
 	lea		Bat2,a1
 	bsr		PwrStartGluebat
+.p3
+	tst.b	Player3Enabled
+	bmi		.exit
 	lea		Bat3,a1
 	bsr		PwrStartGluebat
+.exit
 	rts
